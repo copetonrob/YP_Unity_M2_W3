dd277b19e858f41f8019d190abb7096c6f83f259
copetonrob <robert1357@mail.ru>
2023-12-07 12:59:18 +0300

    Content Added

---
+++ Webinar3Task1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Webinar3Task1 : MonoBehaviour
+{
+    [SerializeField] Light _light;
+    [SerializeField] MeshRenderer _meshRenderer;
+
+    Color _color;
+    float _timer = 0;
+
+    void Update()
+    {
+        _timer = _timer + Time.deltaTime;
+        if (_timer > 3)
+        {
+            _timer = 0;
+        }
+
+        if (_timer < 1)
+        {
+            _color = new Color(1, 0, 0);
+        }
+        else if (_timer < 2)
+        {
+            _color = new Color(0, 1, 0);
+        }
+        else
+        {
+            _color = new Color(0, 0, 1);
+        }
+
+        _light.color = _color;
+        _meshRenderer.material.SetColor("_EmissionColor", _color);
+    }
+}
---
+++ Webinar3Task2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Webinar3Task2 : MonoBehaviour
+{
+    public float Speed = 3f;
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(horizontal, 0f, vertical) * Time.deltaTime * Speed;
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            transform.localScale = new Vector3(1f, 0.5f, 1f);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 clonePosition = transform.position + Random.insideUnitSphere * 3;
+            clonePosition.y = 0;
+            Instantiate(this.gameObject, clonePosition, Quaternion.identity);
+        }
+    }
+}
